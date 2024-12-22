37edcdf45ffe0ad4b22158d7be0ddb0e806c2431
dreamstudio-satheesh <4798470+dreamstudio-satheesh@users.noreply.github.com>
2024-12-22 15:57:51 +0530

    app

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,3 +10,10 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+// Include Admin Routes
+require base_path('routes/admin.php');
+
+// Include Tenant Routes
+require base_path('routes/tenant.php');
