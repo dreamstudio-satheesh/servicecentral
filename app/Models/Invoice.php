de1bd7b786432dab6d57be4d20a00aa4c8c8504d
dreamstudio-satheesh <4798470+dreamstudio-satheesh@users.noreply.github.com>
2024-12-25 12:39:24 +0530

    app

--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -3,8 +3,31 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Invoice extends Model
 {
-    //
+    use HasFactory;
+    
+    protected $fillable = [
+        'store_id',
+        'plan_id',
+        'amount',
+        'status',
+        'issue_date',
+        'due_date',
+    ];
+
+    /**
+     * Relationships
+     */
+    public function store()
+    {
+        return $this->belongsTo(Store::class);
+    }
+
+    public function plan()
+    {
+        return $this->belongsTo(Plan::class);
+    }
 }
